f0104499477acb4f6c2145408f781c101a7ccf12
KH037s <KH037s@KH037-02.uncanet.unca.edu>
2018-10-29 12:50:55 -0400

    Successfully uploaded to Kattis

---
+++ TakeTwoStones/src/TakeTwoStones.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class TakeTwoStones {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		int i = scan.nextInt();
+		
+		if ( i % 2 == 0) {
+			System.out.println("Bob");
+			
+		}else {
+			System.out.println("Alice");
+		}
+
+	}
+
+}
